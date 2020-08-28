dbab20d31f72b8e033444c8772898c2be67347f5
vin-unicity <vinoth.kumar@unicity.com>
2020-08-29 01:38:25 +0530

    Row margin

--- src/Row.js
+++ src/Row.js
@@ -25,7 +25,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
         {movies.map((movie) => (
           <img
             key={movie.id}
-            className="row__poster"
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
             src={`${base_url}${
               isLargeRow ? movie.poster_path : movie.backdrop_path
             }`}
