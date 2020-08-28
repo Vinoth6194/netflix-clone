9029bc210c314eda640318af0a3f26fc9b97d391
vin-unicity <vinoth.kumar@unicity.com>
2020-08-29 00:20:28 +0530

    images are displayed from tmdb

--- src/Row.js
+++ src/Row.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from './axios';
+const base_url = "https://image.tmdb.org/t/p/original/";
 
 function Row({title, fetchUrl}) {
     const [movies, setMovies] = useState([]);
@@ -14,10 +15,18 @@ function Row({title, fetchUrl}) {
         fetchData();
     }, [fetchUrl])
     console.log(movies);
+    //console.table(movies);
     return (
-        <div>
+        <div className = "row">
             {/* titles */}
            <h2>{title}</h2> 
+           <div className = "row__posters">
+                    {
+                        movies.map(movie =>(
+                            <img  src ={`${base_url}${movie.poster_path}`} alt = {movie.name}/>
+    ))
+                    }
+           </div>
            {/* containers -> posters */}
         </div>
     )
