c3a7d248bf9e2aaa9ae336cd610a775e6d46ee99
vin-unicity <vinoth.kumar@unicity.com>
2020-08-29 01:15:05 +0530

    all the genres are displayed

--- src/App.js
+++ src/App.js
@@ -9,6 +9,12 @@ function App() {
       <h1>Vin_Netflix Clone</h1>
       <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOrignals} />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTrending} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
