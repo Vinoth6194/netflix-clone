4dfc47a2e1f37d4fda210c1299c5dee0c25f7ac4
vin-unicity <vinoth.kumar@unicity.com>
2020-08-29 00:38:58 +0530

    code refractoring

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./App.css";
 import Row from "./Row";
-import requests from './requests';
+import requests from "./requests";
 
 function App() {
   return (
     <div className="App">
       <h1>Vin_Netflix Clone</h1>
-      <Row title = "NETFLIX ORIGINALS"  fetchUrl = {requests.fetchNetflixOrignals}/>
-      <Row title = "Trending Now" fetchUrl ={requests.fetchTrending}/>
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOrignals} />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -1,35 +1,33 @@
-import React, { useState, useEffect } from 'react';
-import axios from './axios';
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-function Row({title, fetchUrl}) {
-    const [movies, setMovies] = useState([]);
+function Row({ title, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
 
-    useEffect(() =>{
-        async function fetchData () {
-            const request = await axios.get(fetchUrl);
-            console.log(request.data.results);
-            setMovies(request.data.results);
-            return request;
-        }
-        fetchData();
-    }, [fetchUrl])
-    console.log(movies);
-    //console.table(movies);
-    return (
-        <div className = "row">
-            {/* titles */}
-           <h2>{title}</h2> 
-           <div className = "row__posters">
-                    {
-                        movies.map(movie =>(
-                            <img  src ={`${base_url}${movie.poster_path}`} alt = {movie.name}/>
-    ))
-                    }
-           </div>
-           {/* containers -> posters */}
-        </div>
-    )
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      console.log(request.data.results);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+  console.log(movies);
+  //console.table(movies);
+  return (
+    <div className="row">
+      {/* titles */}
+      <h2>{title}</h2>
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img src={`${base_url}${movie.poster_path}`} alt={movie.name} />
+        ))}
+      </div>
+      {/* containers -> posters */}
+    </div>
+  );
 }
 
-export default Row
+export default Row;
