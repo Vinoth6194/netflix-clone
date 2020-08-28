bc54c218b2d0d261ec9b09e5fcbf539adbd14980
vin-unicity <vinoth.kumar@unicity.com>
2020-08-29 01:28:29 +0530

    Thumbnails and posters

--- src/App.js
+++ src/App.js
@@ -7,9 +7,13 @@ function App() {
   return (
     <div className="App">
       <h1>Vin_Netflix Clone</h1>
-      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOrignals} />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOrignals}
+        isLargeRow
+      />
       <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
-      <Row title="Top Rated" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
       <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
       <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
       <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
