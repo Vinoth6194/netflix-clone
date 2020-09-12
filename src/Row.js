ab4b5b3db03fc9d099943d66be93ee41289c4d93
vin-unicity <vinoth.kumar@unicity.com>
2020-09-12 23:02:30 +0530

    trailer embedded

--- src/Row.js
+++ src/Row.js
@@ -1,11 +1,33 @@
 import React, { useState, useEffect } from "react";
 import "./Row.css";
-
+import Youtube from "react-youtube";
 import axios from "./axios";
+import movieTrailer from "movie-trailer";
+
 const base_url = "https://image.tmdb.org/t/p/original/";
 function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
-
+  const [trailerUrl, setTrailerUrl] = useState("");
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log("this is error"));
+    }
+  };
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchUrl);
@@ -16,6 +38,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
     fetchData();
   }, [fetchUrl]);
   console.log(movies);
+
   //console.table(movies);
   return (
     <div className="row">
@@ -25,6 +48,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
         {movies.map((movie) => (
           <img
             key={movie.id}
+            onClick={() => handleClick(movie)}
             className={`row__poster ${isLargeRow && "row__posterLarge"}`}
             src={`${base_url}${
               isLargeRow ? movie.poster_path : movie.backdrop_path
@@ -34,6 +58,7 @@ function Row({ title, fetchUrl, isLargeRow }) {
         ))}
       </div>
       {/* containers -> posters */}
+      {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
